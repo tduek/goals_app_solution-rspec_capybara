730de5e37a0d63d4a186958c5bf540439ff32366
Tommy Duek <taduek@gmail.com>
2013-11-18 15:23:27 -0500

    Initial commit. Sets up test framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoalsApp::Application.config.secret_token = '3ff26bb0d19f73ce91c0d16419d7de271d19f9c4bcf92f7e8078aa835d742d8a0de74a432b645035ff995c2592b4383b76650fbcc01e9b5a0f36c20a205b0b5d'
