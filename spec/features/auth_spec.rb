b1d6c14e43e15cfd9580717b362f7de299efaa5e
Tommy Duek <taduek@gmail.com>
2013-11-18 15:50:26 -0500

    Start TDDing login integration.

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "the signup process" do
+
+  it "has a new user page"
+
+  describe "signing up a user" do
+
+    it "shows username on the homepage after signup"
+
+  end
+
+end
+
+describe "logging in" do
+
+  it "shows username on the homepage after login"
+
+end
+
+describe "logging out" do
+
+  it "begins with logged out state"
+
+  it "doesn't show username on the homepage after logout"
+
+end
